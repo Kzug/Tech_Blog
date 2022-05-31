70731b55a9e817204d9834c4b345e6749532d3ac
Kiersten Zug <kierstenzug@gmail.com>
2022-05-31 13:08:36 -0700

    set up all handlebar pages

---
+++ public/js/profile.js
@@ -0,0 +1,46 @@
+const newFormHandler = async (event) => {
+  event.preventDefault();
+
+  const name = document.querySelector("#blog-name").value.trim();
+  const entry = document.querySelector("#blog-entry").value.trim();
+
+  if (name && entry) {
+    const response = await fetch(`/api/blogs`, {
+      method: "POST",
+      body: JSON.stringify({ name, entry }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (response.ok) {
+      document.location.replace("/profile");
+    } else {
+      alert("Failed to create blog");
+    }
+  }
+};
+
+const delButtonHandler = async (event) => {
+  if (event.target.hasAttribute("data-id")) {
+    const id = event.target.getAttribute("data-id");
+
+    const response = await fetch(`/api/blogs/${id}`, {
+      method: "DELETE",
+    });
+
+    if (response.ok) {
+      document.location.replace("/profile");
+    } else {
+      alert("Failed to delete blog");
+    }
+  }
+};
+
+document
+  .querySelector(".new-blog-entry")
+  .addEventListener("submit", newFormHandler);
+
+document
+  .querySelector(".blog-list")
+  .addEventListener("click", delButtonHandler);
