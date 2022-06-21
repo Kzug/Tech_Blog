8ba73aa2edc2c4756ad931c464174b6d5e77fbcb
Kiersten Zug <99204084+Kzug@users.noreply.github.com>
2022-06-20 20:36:12 -0700

    heroku testing

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -4,15 +4,19 @@ const withAuth = require("../../utils/auth");
 
 router.post("/", withAuth, async (req, res) => {
   // console.log(req.body);
+  console.log("Test One");
   try {
     const newComment = await Comment.create({
       ...req.body,
       user_id: req.session.user_id,
     });
+    console.log("Test Two");
 
     res.status(200).json(newComment);
+    console.log("Test Three");
   } catch (err) {
     res.status(400).json(err);
+    console.log("Test Four");
   }
 });
 
